d9828063332d1a1b19dc7c6538b011e781da7967
Anton Tymoshchuk <aatymoshchuk@gmail.com>
2020-10-02 13:56:46 +0300

    Add project files.

---
+++ User/Models/Content.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace User.Models
+{
+    public class Content : INotifyPropertyChanged
+    {
+        private ObservableCollection<Element> elements;
+        private int directoriesCount;
+        private int filesCount;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public ObservableCollection<Element> Elements
+        {
+            get { return elements; }
+            set
+            {
+                elements = value;
+                OnPropertyChanged("Elements");
+            }
+        }
+
+        public int DirectoriesCount
+        {
+            get { return directoriesCount; }
+        }
+
+        public int FilesCount
+        {
+            get { return filesCount; }
+        }
+
+        public int Total
+        {
+            get { return directoriesCount + filesCount; }
+        }
+
+        public Content()
+        {
+            elements = new ObservableCollection<Element>();
+
+            DirectoryInfo directoryInfo = new DirectoryInfo(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments));
+            GetDirectoryContent(directoryInfo);
+
+            directoryInfo = new DirectoryInfo(Environment.GetFolderPath(Environment.SpecialFolder.MyMusic));
+            GetDirectoryContent(directoryInfo);
+
+            directoryInfo = new DirectoryInfo(Environment.GetFolderPath(Environment.SpecialFolder.MyPictures));
+            GetDirectoryContent(directoryInfo);
+
+            directoryInfo = new DirectoryInfo(Environment.GetFolderPath(Environment.SpecialFolder.MyVideos));
+            GetDirectoryContent(directoryInfo);
+
+            directoryInfo = new DirectoryInfo(Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory));
+            GetDirectoryContent(directoryInfo);
+        }
+
+        private void GetDirectoryContent(DirectoryInfo directoryInfo)
+        {
+            try
+            {
+                DirectoryInfo[] directoryInfos = directoryInfo.GetDirectories();
+                for (int i = 0; i < directoryInfos.Length; i++)
+                {
+                    elements.Add(new Element(directoryInfos[i].FullName));
+                    GetDirectoryContent(directoryInfos[i]);
+                    directoriesCount++;
+                }
+
+                FileInfo[] fileInfos = directoryInfo.GetFiles();
+                for (int i = 0; i < fileInfos.Length; i++)
+                {
+                    elements.Add(new Element(fileInfos[i].FullName));
+                    filesCount++;
+                }
+            }
+            catch { }
+        }
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ User/Models/Element.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace User.Models
+{
+    public class Element : INotifyPropertyChanged
+    {
+        private string path;
+        private string icon;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private Exception ElementNotFoundException = new Exception("Element not found");
+
+        public string Path
+        {
+            get { return path; }
+            set
+            {
+                path = value;
+                OnPropertyChanged("Path");
+            }
+        }
+
+        public string Icon
+        {
+            get { return icon; }
+            set
+            {
+                icon = value;
+                OnPropertyChanged("Icon");
+            }
+        }
+
+        public Element(string path)
+        {
+            FileInfo fileInfo = new FileInfo(path);
+            DirectoryInfo directoryInfo = new DirectoryInfo(path);
+
+            if (fileInfo.Exists == true)
+            {
+                this.path = path;
+                icon = "/Images/icons8-document-36.png";
+            }
+            else if (directoryInfo.Exists == true)
+            {
+                this.path = path;
+                icon = "/Images/icons8-folder-36.png";
+            }
+            else throw ElementNotFoundException;
+        }
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
